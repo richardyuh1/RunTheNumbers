5cd3fb375844266336a1c4629b59c5d03fafe669
Richard <richardyuh1@gmail.com>
2019-11-11 17:13:27 -0800

    Fixed urllib import problem

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 from datetime import date 
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd 
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
 
 app = Flask(__name__)
 
